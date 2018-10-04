559137f3ea455c6aa787b7b55e27f304aad118ba
Rodrigo Rosa <proff.rodrigo.rosa@gmail.com>
2018-10-03 23:46:11 -0300

    Exception on DAOlogin fixed
    Exception on DAOlogin fixed

--- src/LoginCheck.java
+++ src/LoginCheck.java
@@ -1,7 +1,4 @@
-import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,47 +15,61 @@ public class LoginCheck extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public LoginCheck() {
+    public LoginCheck() {//construtor padrão
         super();
     }
     
+    //declaração das variaveis q vão receber as variaveis q estão vindo como parametro das requisições abaixo
     private String uname;
 	private String password;
   
-	
+    String[] vetor = null;//vetorzinho para trabalhar o retorno da query feita la no DAO
+    
 	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    //metodo caso haja uma requisição do tipo get, e não usei ela pq eu n tenho req do tipo get
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	//usei o post, pq o form login la da tela INDEX.JSP é do tipo post
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		this.uname=request.getParameter("uname");
-		this.password=request.getParameter("password");
 		
-		/*if (getUname().equals("adm") && getPassword().equals("1234")){
-			response.sendRedirect("Home_adm.jsp");
-		}
-		else{
-			response.sendRedirect("error.jsp");
+		this.uname=request.getParameter("uname");//parametro uname sendo salvo na variavel dessa classe uname
+		
+		this.password=request.getParameter("password");//parametro password sendo salvo na variavel dessa classe password
+		
+		
+		LoginDAO login = new LoginDAO(uname);//instancia e envio da variavel dessa classe uname la pra classe DAO
+		
+		//System.out.println(l.procurar_id()[0]); //teste com funcao q retorna um vetor
+		
+		this.vetor= login.procurar_id();//armazena o retorno da funcao que é um vetor no vetor local
+		
+		/*for (int j = 0; j < vetor.length; j++) {
+			System.out.println(vetor[j]);
 		}*/
 		
-		LoginDAO l = new LoginDAO();
-		
-		//System.out.println(l.procurar_id());
-		
-		if (uname.equals("admin") && password.equals("1234") && (l.procurar_id() != null)){
+		//comparações da variavel uname(contem o retorno do doPOST) com o vetor (contem o retorno do banco de dados)
+		if (uname.equals(vetor[1]) && password.equals(vetor[2]) && (vetor[3].equals("1"))){
 			response.sendRedirect("Home_adm.jsp");
+			//direciona para o jsp adm caso o vetor[3] seja igual a 1
+		}
+		else if(uname.equals(vetor[1]) && password.equals(vetor[2]) && (vetor[3].equals("0"))){
+			response.sendRedirect("Home_Cliente.jsp");
+			//direciona para o jsp cliente caso o vetor[3] seja igual a 0
 		}
 		else{
 			response.sendRedirect("error.jsp");
+			//erro de autenticacao
 		}
+		
+		
 	}
 
 }
--- src/LoginDAO.java
+++ src/LoginDAO.java
@@ -4,61 +4,80 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 
 
 public class LoginDAO {
 	
 	Connection conexao;
+	String uname;
     
-    public LoginDAO(){
+    public LoginDAO(String uname){
+    	
+    	this.uname = uname;
+    	
     	
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            //Class.forName("org.mariadb.jdbc.Driver");
-            /* Abertura de conexão: */
-            /* Notar que poderia ser substituida por
-            uma chamada de get instance de uma conexão já aberta. 
-            */
-            //conexao = DriverManager.getConnection("jdbc:mariadb://172.16.7.63:3306/agenda","usuario", "usuario123");
+            
             conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/login?"+"user=root&password=root");
 
             System.out.print("Conexao ok\n");
             
         } catch (Exception e){
-            System.out.print("Erro de conexao LoginDAO");
+            System.out.print("Erro de conexao LoginDAO "+e.toString());
         }
     }
     
-    public String procurar_id() {
-    	
-        try{        
-            PreparedStatement comandoSQLp = conexao.prepareStatement("select * from login.login where id = ?");  
-            comandoSQLp.setString(1, "1");
+    public String[] procurar_id() {
+        try{
+            PreparedStatement comandoSQLp = conexao.prepareStatement("select * from login.login where login = ?");
+            //sql query
+            comandoSQLp.setString(1, uname);//substitui o 1� ? pela variavel uname
             ResultSet rs = comandoSQLp.executeQuery();
             System.out.println("Conectei a tabela login");
             
-            rs.next();
+            //rs.next();//se for testar o rs.next() com o print, deixar apenas um se n da erro
+            
+            //System.out.println(rs.next()); //se for testar o rs.next() com o print, deixar apenas um se n da erro
             
             //System.out.println(rs.getString(2));
             
-            String id = rs.getString(1);
-            String name = rs.getString(2);
-            String password = rs.getString(3);
             
+            String consulta[] = new String[4];
+            
+            if (rs.next()==true) { // S� PODE CHAMAR O RS.NEXT() UMA VEZ!!!!!!!!!!!!!!!!!!!!!!!
+            	//copula os setores do vetor consulta com o parametro corresposnte ao retorn sql
+            	consulta[0] = rs.getString(1);
+    	        consulta[1] = rs.getString(2);
+    	        consulta[2] = rs.getString(3);
+    	        consulta[3] = rs.getString(4);
+			} else {// caso o rs.next() fo false, ou seja, a consulta sql n�o restornou positivo, copula os setores com nada
+				consulta[0] = "";
+    	        consulta[1] = "";
+    	        consulta[2] = "";
+    	        consulta[3] = "";	
+			}
+            
+	        
+
             /* Se utilizar o padrão singleton, não fechar a conexão. */
             comandoSQLp.close();
+            
             rs.close();
             //conexao.close();
             
             //return id +" "+name+" "+password;
-            return id;
+                  
+            return consulta;// retorna o vetor carregado com os resultados da busca sql ou vazio
         }           
         catch (Exception e)
         {
-          System.out.print("\nErro de conexao...\nFind by id usuario " + e.toString());
+          System.out.print("\nErro de conexao LoginDAO...\nFind by id usuario " + e.toString());
         }
-        return null;
-        
+        return null;  
     }
 	
 }
